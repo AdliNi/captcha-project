1a5dbf5a74b6949723ba022462f4ce6b792f4436
AdliNi <2022484504@student.uitm.edu.my>
2025-07-07 04:12:55 +0800

    Dummyscripts.js

--- DummyScript.js
+++ DummyScript.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Initial content
     console.log("Page Loaded");
-    console.log("test");
 
     // You can leave this empty, as the extension will modify the page.
-    //test
 });
