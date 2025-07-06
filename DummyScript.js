4d01a98f6e3cf0a688257dd43baf5fcd0d2288d8
AdliNi <2022484504@student.uitm.edu.my>
2025-07-07 03:53:31 +0800

    Update DummyScript.js GUI logic

--- DummyScript.js
+++ DummyScript.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Initial content
     console.log("Page Loaded");
+    console.log("test");
 
     // You can leave this empty, as the extension will modify the page.
+    //test
 });
